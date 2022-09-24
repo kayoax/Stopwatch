e6e9b54b46b5a41bd16769516e5cc97bb50eb6b7
Kayo Cordeiro <kayo@outlook.in>
2022-09-24 11:06:53 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+static void Menu(){
+  Console.Clear();
+  Console.WriteLine("S - Segundos => 13s = 13 segundos");
+  Console.WriteLine("M - Minutos => 1m = 1 minuto");
+  Console.WriteLine("X - Sair");
+  Console.WriteLine("Selecione uma opção: ");
+  
+  string data = Console.ReadLine()!.ToLower();
+  if (data == "x") System.Environment.Exit(0);
+  char type = char.Parse(data.Substring(data.Length - 1, 1));
+  int time = int.Parse(data.Substring(0, data.Length - 1));
+  int multiplier = 1;
+  
+  if (type == 'm') multiplier = 60;
+
+  Start(time * multiplier);
+}
+
+Menu();
+
+static void PreStart(){
+  Console.Clear();
+  Console.WriteLine("Preparar..");
+  Thread.Sleep(1000);
+  Console.Clear();
+  Console.WriteLine("Apontar..");
+  Thread.Sleep(1000);
+  Console.Clear();
+  Console.WriteLine("Começou!");
+  Thread.Sleep(1000);
+}
+
+static void Start(int time){
+  PreStart();
+
+  while(time > 0){
+    Console.Clear();
+    Console.WriteLine(time);
+    Thread.Sleep(1000);
+    time--;
+  }
+
+  Console.Clear();
+  Console.WriteLine("Stopwatch finalizado!");
+  Thread.Sleep(2500);
+  Menu();
+}
